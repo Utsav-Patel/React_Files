110a119ebff3f11ee764ca5b9dcb7adc83bba79e
Utsav-Patel <utsav.kalash131297@gmail.com>
2018-05-27 13:19:42 +0530

    Adding LifeCycle Method to a class

--- src/index.js
+++ src/index.js
@@ -2,29 +2,43 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 
-const user = {firstname: 'Utsav', lastname: 'Patel'};
+const user = {firstName:'Utsav', LastName:'Patel'};
 
 function formatName(user){
-  return user.firstname+" "+user.lastname;
+  return user.firstName + " " + user.LastName;
 }
 
-function tick(){
-  const element = (
-    <div>
-      Hello
-      <h2>
-        {formatName(user)}
-      </h2>
-      <h3>
-        It is {new Date().toLocaleTimeString()}.
-      </h3>
-    </div>
-  );
+class Clock extends React.Component{
+  constructor(props){
+    super(props);
+    this.state = {date: new Date()};
+  }
 
-  ReactDOM.render(
-    element,
-    document.getElementById(`root`)
-  );
+  tick(){
+    this.setState({
+      date: new Date()
+    });
+  }
+
+  componentDidMount(){
+    this.timeId = setInterval(() => this.tick(),1000);
+  }
+
+  componentWillUnmount(){
+    clearInterval(this.timeId);
+  }
+
+  render(){
+    return (
+      <div>
+        <h2> Hello {formatName(user)} </h2>
+        <h3> It is {this.state.date.toLocaleTimeString()} </h3>
+      </div>
+    );
+  }
 }
 
-setInterval(tick,1000);
+ReactDOM.render(
+  <Clock />,
+  document.getElementById('root')
+);
