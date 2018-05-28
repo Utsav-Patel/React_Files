ee6c753d68f8760be2c69fa21b0dd312603ce381
Utsav-Patel <utsav.kalash131297@gmail.com>
2018-05-28 14:35:21 +0530

    If statement and conditional operator

--- src/index.js
+++ src/index.js
@@ -8,29 +8,67 @@ function formatName(user){
   return user.firstName + " " + user.LastName;
 }
 
-class Toggle extends React.Component{
+function Login(props){
+  return <button onClick={props.onClick}>Login</button>;
+}
+
+function Logout(props){
+  return <button onClick={props.onClick}>Logout</button>;
+}
+
+function UserLogin(){
+  return <h1>Welcome Back!</h1>;
+}
+
+function UserLogout(){
+  return <h1>Please Login</h1>;
+}
+
+function Greeting(props){
+  if(props.isLogin)
+    return <UserLogin />;
+  else
+    return <UserLogout />;
+}
+
+class Log extends React.Component{
   constructor(props){
     super(props);
-    this.state = {isToggleOn: true};
-    this.handle = this.handleClick.bind(this);
+    this.state = {isLogin: false};
+    this.handleLogin = this.handleLogin.bind(this);
+    this.handleLogout = this.handleLogout.bind(this);
+    // this.Login = this.Login.bind(this);
+    // this.Logout = this.Logout.bind(this);
+    // this.UserLogin = this.UserLogin.bind(this);
+    // this.UserLogout = this.UserLogout.bind(this);
+    // Greeting = Greeting.bind(this);
   }
 
-  handleClick(){
-    this.setState(prevState => ({
-      isToggleOn: !prevState.isToggleOn
-    }));
+  handleLogin(){
+    this.setState({isLogin: true});
+  }
+
+  handleLogout(){
+    this.setState({isLogin: false});
   }
 
   render(){
+    const isLogin = this.state.isLogin;
+    var button = null;
+    if(isLogin)
+      button = <Logout onClick={this.handleLogout} />;
+    else
+      button = <Login onClick={this.handleLogin} />;
     return (
-      <button onClick={this.handle}>
-        {this.state.isToggleOn ? 'ON' : 'OFF'}
-      </button>
+      <div>
+        <Greeting isLogin={isLogin} />
+        {button}
+      </div>
     );
   }
 }
 
 ReactDOM.render(
-  <Toggle />,
+  <Log />,
   document.getElementById('root')
 );
